22f5afabd63d0e4731bc83845d275424dca86222
seilor0 <seilor0001@gmail.com>
2026-05-17 17:05:09 +0900

    Merge branch 'refactor/use-class-in-spite-of-dic'

--- code_vue.js
+++ code_vue.js
@@ -3,6 +3,9 @@ import ButtonCssIcon from "./components/button-css-icon.js";
 import MultiSelect from "./components/multi-select.js";
 import ResultCard from "./components/result-card.js";
 
+import {ChatData, CocRollData, EmokloreRollData} from './components/class.js'
+import {floatRound} from '../__utility/function.js'
+
 const {createApp, ref, computed, watch, onMounted, toRaw} = Vue;
 
 const rootApp = createApp({
@@ -15,7 +18,7 @@ const rootApp = createApp({
 
   setup () {
     const system = ref('coc6th');
-    const isCoC = computed(()=>system.value.startsWith('coc'));
+    const isCoc = computed(()=>system.value.startsWith('coc'));
     const setting = ref({
       show: {selectTag: false, total: true, diceLog: false},
 
@@ -32,8 +35,7 @@ const rootApp = createApp({
       coc7th: new Map(),
     });
 
-
-    /** { tab, name, nameNodup, log } [] */
+    /** ChatData [] */
     const chatArr = ref([]);
 
     async function readHtmlFile(e, add=false) {
@@ -62,21 +64,22 @@ const rootApp = createApp({
 
         const tab  = arr[0].innerText.trim().slice(1,-1);
         const name = arr[1].innerText.replaceAll('　',' ').trim();
-        const nameNodup = name.replace(/ *\(\d+\)$/,'');
         const log  = normStr(arr[2].innerHTML).trim();
 
         const textArr = log.split(/#\d+/);
         if (textArr.length===1) {
-          chatArr.value.push({tab:tab, name:name, nameNodup:nameNodup, log:log});
+          chatArr.value.push(new ChatData({tab:tab, name:name, log:log}));
         } else {
           const head = textArr.shift();
-          textArr.forEach(t => chatArr.value.push({tab:tab, name:name, nameNodup:nameNodup, log:`${head}${t.replaceAll('\n',' ')}`.trim()}));
+          textArr.forEach(t => 
+            chatArr.value.push(new ChatData({tab:tab, name:name, log:`${head}${t.replaceAll('\n',' ')}`.trim()}))
+          );
         }
       });
       return;
 
       function normStr(str) {
-        let string = [
+        const string = [
           ['　',' '],
           ['&lt;', '<'],
           ['&gt;', '>'],
@@ -86,22 +89,18 @@ const rootApp = createApp({
           ['&#x60;', '`'],
           ['<br>', '\n'],
           ['\t','  '],
-          [/[！-｝]/g, function(s){return String.fromCharCode(s.charCodeAt(0) - 0xFEE0)}],
+          [/[！-｝]/g, (s)=>String.fromCharCode(s.charCodeAt(0) - 0xFEE0)],
         ].reduce((ac,cur) => ac = ac.replaceAll(cur[0], cur[1]), str);
         return string;
       }
     }
 
-    /** {
-     * rollNo, tab, name, nameNodup, log, diceVal, skill, threshold
-     *  coc      : diceVal, CF
-     *  emoklore : D, C, E, diceVal[], suc, sucE
-    } [] */
+    /** CocRollData | EmokloreRollData [] */
     const rollArr = computed(() => {
       let id = 0;
       const resultArr  = [];
 
-      const dicePat = isCoC.value ? / > (?<diceVal>\d+)\D* > / : / > \[(?<diceVal>[\d, ]+)\] > [-\d]/;
+      const dicePat = isCoc.value ? / > (?<diceVal>\d+)\D* > / : / > \[(?<diceVal>[\d, ]+)\] > [-\d]/;
       const replaceArr = [
         [/[【】「」『』《》〈〉〔〕\[\]]/g, ''],
         [/[<≪](.*?)[>≫]/g, '$1'],
@@ -111,110 +110,112 @@ const rootApp = createApp({
         [')', '）'],
         ['!', '！'],
         ['?', '？']
-      ].concat(isCoC.value ? [['*',' × ']] : []);
+      ].concat(isCoc.value ? [['*',' × ']] : []);
 
       chatArr.value
-      .filter(dic => (isCoC.value ? /.\(1d100<=/is : /DA.+\(\d+DA|<=.+\(\d+DM<=/is).test(dic.log))
-      .forEach(dic => {
+      .filter(chatData => (isCoc.value ? /.\(1d100<=/is : /DA.+\(\d+DA|<=.+\(\d+DM<=/is).test(chatData.log))
+      .forEach(chatData => {
         // rollNo, タブ, キャラクター, ログ
-        const rollDic = {rollNo:id++, ...dic};
+        const rollData = isCoc.value ? 
+          new CocRollData({rollNo:id++, ...chatData}) : 
+          new EmokloreRollData({rollNo:id++, ...chatData});
 
         // 出目
-        let {diceVal} = dic.log.match(dicePat).groups;
+        let {diceVal} = chatData.log.match(dicePat).groups;
         if (!diceVal) return;
-        diceVal = isCoC.value ? parseInt(diceVal) : diceVal.split(',').map(e=>parseInt(e));
-        rollDic.diceVal = diceVal;
+        diceVal = isCoc.value ? parseInt(diceVal) : diceVal.split(',').map(e=>parseInt(e));
+        rollData.diceVal = diceVal;
 
         // 技能名
         let skillPat;
-        if (isCoC.value) {
-          skillPat = /CBR|RES/i.test(dic.log) ?
+        if (isCoc.value) {
+          skillPat = /CBR|RES/i.test(chatData.log) ?
             /\d+\) (?<skill>.*)\(1d100<=/si :
             /<=.+? (?<skill>.*)\(1d100<=/si;
         } else {
-          skillPat = /\d+DA\d+/i.test(dic.log) ?
+          skillPat = /\d+DA\d+/i.test(chatData.log) ?
             /DA.+? (?<skill>.*)\(\d+DA\d+\)/si :
             /<=.+? (?<skill>.*)\(\d+DM<=\d+\)/si;
         }
 
-        const {skill} = dic.log.match(skillPat)?.groups;
-        rollDic.skill = replaceArr
+        const {skill} = chatData.log.match(skillPat)?.groups ?? {skill:''};
+        rollData.skill = replaceArr
           .reduce((ac, cur)=>ac=ac.replaceAll(cur[0],cur[1]), skill)
           .trim();
 
         // 判定値・C/F
         if (system.value==='coc6th') {
           // 組み合わせロール
-          if (/ > \d+\[.+,.+\] > /.test(dic.log)) {
-            rollDic.threshold = Math.min(...dic.log.match(/<=([\d,]+)\) > /)[1].split(','));
-            const text = dic.log.match(/ > \d+\[(.+,.+)\] > /)[1];
-            rollDic.CF =
+          if (/ > \d+\[.+,.+\] > /.test(chatData.log)) {
+            rollData.threshold = Math.min(...chatData.log.match(/<=([\d,]+)\) > /)[1].split(','));
+            const text = chatData.log.match(/ > \d+\[(.+,.+)\] > /)[1];
+            rollData.CF =
               text.includes('決定的成功') ? 'C' :
               text.includes('スペシャル') ? 'S' :
               text.includes('致命的失敗') ? 'F' : null;
           } else {
-            rollDic.threshold = parseInt(dic.log.match(/\(1d100<=([-\d]+)\).* > /i)[1]);
-            rollDic.CF =
-              /決定的成功(\/スペシャル)?$/.test(dic.log) ? 'C' :
-              dic.log.endsWith('スペシャル') ? 'S' :
-              dic.log.endsWith('致命的失敗') ? 'F' : null;
+            rollData.threshold = parseInt(chatData.log.match(/\(1d100<=([-\d]+)\).* > /i)[1]);
+            rollData.CF =
+              /決定的成功(\/スペシャル)?$/.test(chatData.log) ? 'C' :
+              chatData.log.endsWith('スペシャル') ? 'S' :
+              chatData.log.endsWith('致命的失敗') ? 'F' : null;
           }
 
         } else if (system.value==='coc7th') {
-          rollDic.threshold = parseInt(dic.log.match(/\(1d100<=([-\d]+)\).* > /i)[1]);
-          rollDic.CF =
-            dic.log.endsWith('クリティカル') ? 'C' :
-            dic.log.endsWith('イクストリーム成功') ? 'Ex' :
-            dic.log.endsWith('ファンブル') ? 'F' : null;
+          rollData.threshold = parseInt(chatData.log.match(/\(1d100<=([-\d]+)\).* > /i)[1]);
+          rollData.CF =
+            chatData.log.endsWith('クリティカル') ? 'C' :
+            chatData.log.endsWith('イクストリーム成功') ? 'Ex' :
+            chatData.log.endsWith('ファンブル') ? 'F' : null;
 
         } else if (system.value==='emoklore') {
-          const D = diceVal.length;
-          const C = diceVal.filter(e=>e===1).length;
-          const E = diceVal.filter(e=>e===10).length;
-          const threshold = parseInt(dic.log.match(/\(\d+DM<=(\d+)\) > \[[\d, ]+\]/i)[1]);
-          rollDic.D = D;
-          rollDic.C = C;
-          rollDic.E = E;
-          rollDic.threshold = threshold;
-          rollDic.suc = C - E + diceVal.filter(e=>e<=threshold).length;;
-          rollDic.sucE = D * threshold / 10;
+          rollData.threshold = parseInt(chatData.log.match(/\(\d+DM<=(\d+)\) > \[[\d, ]+\]/i)[1]);
         }
 
-        resultArr.push(rollDic);
+        resultArr.push(rollData);
       });
 
       console.log('compute roll arr', resultArr);
       return resultArr;
     });
 
-    const skillSet = computed(() => new Set(rollArr.value.map(dic=>dic.skill).filter(Boolean).sort()));
+    const skillSet = computed(() => new Set(rollArr.value.map(rollData=>rollData.skill).filter(Boolean).sort()));
     
     const tabMap = ref(new Map());
     watch(rollArr, ()=> {
       tabMap.value.clear();
-      new Set(rollArr.value.map(dic=>dic.tab)).forEach(key=>tabMap.value.set(key,true));
+      new Set(rollArr.value.map(rollData=>rollData.tab)).forEach(key=>tabMap.value.set(key,true));
     });
 
     /** name1 : {show:true, characters:[name1,name2,name3]} */
     const nameUnifiedMap = ref(new Map());
     watch(() => [rollArr.value, setting.value.mergeDupUnit], () => {
       nameUnifiedMap.value.clear();
-      const nameSet = new Set(rollArr.value.map(dic=>setting.value.mergeDupUnit ? dic.nameNodup : dic.name).sort());
-      nameSet.forEach(name=>nameUnifiedMap.value.set( name, {show:true, characters:new Set([name])} ));
+      const nameSet = new Set(
+        rollArr.value.map(rollData=>rollData[setting.value.mergeDupUnit ? 'nameNodup' : 'name']).sort()
+      );
+      nameSet.forEach(name => 
+        nameUnifiedMap.value.set( name, {show:true, characters:new Set([name])} )
+      );
     });
+    /**
+     * @param {String} character 統合先のキャラクター名
+     * @param {String} target 統合元のキャラクター名
+     * @param {Boolean} newState 統合元の新しい表示状態
+     */
     function updateNameUnify (character, target, newState) {
       nameUnifiedMap.value.get(target).show = !newState;
-      const targetSet = nameUnifiedMap.value.get(character).characters;
-      const changeNames = nameUnifiedMap.value.get(target).characters;
+      const targetSet   = toRaw(nameUnifiedMap.value.get(character).characters);
+      const changeNames = toRaw(nameUnifiedMap.value.get(target).characters);
       nameUnifiedMap.value.get(character).characters = newState ? 
-        toRaw(targetSet).union     (toRaw(changeNames)):
-        toRaw(targetSet).difference(toRaw(changeNames));
-      console.log(nameUnifiedMap.value);
+        targetSet.union     (changeNames):
+        targetSet.difference(changeNames);
+      // console.log(nameUnifiedMap.value);
     }
 
 
     const diceLogHead = computed(() => {
-      if (isCoC.value) {
+      if (isCoc.value) {
         if (setting.value.showStyle==='log') return ['タブ', 'ログ', null];
         else return ['タブ', '技能', '判定値', '出目', null];
       } else {
@@ -226,21 +227,23 @@ const rootApp = createApp({
     const resultDataMap = computed(() => {
       const resultMap = new Map();
       nameUnifiedMap.value.forEach((value, key) => {
-        const targetRoll = structuredClone(rollArr.value)
-          .filter(dic=>tabMap.value.get(dic.tab))
-          .filter(dic=>value.characters.has(setting.value.mergeDupUnit ? dic.nameNodup : dic.name));
-        resultMap.set(key, {show:value.show, ...makeResultData(targetRoll)});
+        const targetRoll = rollArr.value
+          .filter(rollData => tabMap.value.get(rollData.tab))
+          .filter(rollData => value.characters.has(rollData[setting.value.mergeDupUnit ? 'nameNodup' : 'name']));
+        resultMap.set(key, {show:value.show, ...computeResultData(targetRoll)});
       });
       console.log('result data map', resultMap);
       return resultMap;
     });
 
     const allResultData = computed(()=> {
-      const allTargetRoll = structuredClone(rollArr.value).filter(dic=>tabMap.value.get(dic.tab));
-      return {show:true, ...makeResultData(allTargetRoll)};
+      const allTargetRoll = rollArr.value.filter(rollData => tabMap.value.get(rollData.tab));
+      return {show:true, ...computeResultData(allTargetRoll)};
     })
 
-    function makeResultData (targetRoll) {
+    function computeResultData (targetRoll) {
+      targetRoll = targetRoll.map(rollData => rollData.clone());
+
       /**
        * coc      : [roll, average, C, F, S/Ex,  Init]
        * emoklore : [roll, D, average, C, E, Suc, SucEx]
@@ -254,41 +257,41 @@ const rootApp = createApp({
       // ロール数
       totalData.set('ロール', targetRoll.length);
 
-      if (isCoC.value) {
+      if (isCoc.value) {
         // 出目平均
         const average = floatRound(targetRoll.reduce((ac,cur)=>ac+cur.diceVal, 0) / targetRoll.length, 2) || null;
         totalData.set('出目平均', average);
 
         // Critial
-        const cArr = targetRoll.filter(dic => {
+        const cArr = targetRoll.filter(rollData => {
           switch(setting.value.totalStyle) {
-            case 'log': return dic.CF==='C';
-            case 'cc' : return dic.diceVal<2;
-            case 'ccb': return dic.diceVal<6;
+            case 'log': return rollData.CF==='C';
+            case 'cc' : return rollData.diceVal<2;
+            case 'ccb': return rollData.diceVal<6;
           }
         });
         totalData.set('Critical', cArr.length);
         if (setting.value.growTarget.includes('C')) {
-          cArr.forEach(dic => dic.CF='C');
+          cArr.forEach(rollData => rollData.CF='C');
           diceLogBaseArr.push(...cArr);
         }
 
         // Fumble
-        const fArr = targetRoll.filter(dic => {
+        const fArr = targetRoll.filter(rollData => {
           switch(setting.value.totalStyle) {
-            case 'log': return dic.CF==='F';
-            case 'cc' : return dic.diceVal>99;
-            case 'ccb': return dic.diceVal>95;
+            case 'log': return rollData.CF==='F';
+            case 'cc' : return rollData.diceVal>99;
+            case 'ccb': return rollData.diceVal>95;
           }
         });
         totalData.set('Fumble', fArr.length);
         if (setting.value.growTarget.includes('F')) {
-          fArr.forEach(dic => dic.CF='F');
+          fArr.forEach(rollData => rollData.CF='F');
           diceLogBaseArr.push(...fArr);
         }
 
         // Special
-        const sArr = targetRoll.filter(dic => dic.CF===(system.value==='coc6th' ? 'S' : 'Ex'));
+        const sArr = targetRoll.filter(rollData => rollData.CF===(system.value==='coc6th' ? 'S' : 'Ex'));
         totalData.set(system.value==='coc6th' ? 'Special' : 'Extreme', sArr.length);
         if (setting.value.growTarget.includes('S')) diceLogBaseArr.push(...sArr);
 
@@ -296,20 +299,20 @@ const rootApp = createApp({
         let count = 0;
         const initSkill = initSkills.value[system.value];
         targetRoll
-          .filter(dic => dic.diceVal <= dic.threshold)
-          .forEach(dic => {
-            const skill = initSkill.keys().find(key => dic.skill.includes(key));
+          .filter(rollData => rollData.diceVal <= rollData.threshold)
+          .forEach(rollData => {
+            const skill = initSkill.keys().find(key => rollData.skill.includes(key));
             if (!skill) return;
-            if (dic.threshold > initSkill.get(skill)) return;
+            if (rollData.threshold > initSkill.get(skill)) return;
 
             count++;
 
             if (setting.value.growTarget.includes('I')) {
               // 初期値成功がC/Sでもある場合
-              if (diceLogBaseArr.includes(dic)) dic.CF += '/初期値';
+              if (diceLogBaseArr.includes(rollData)) rollData.CF += '/初期値';
               else {
-                dic.CF = '初期値';
-                diceLogBaseArr.push(dic);
+                rollData.CF = '初期値';
+                diceLogBaseArr.push(rollData);
               }
             }
           });
@@ -318,8 +321,8 @@ const rootApp = createApp({
         // selected skill
         if (setting.value.growTarget.includes('E') && setting.value.selectedSkills.length) {
           const selectedArr = targetRoll
-            .filter(dic => setting.value.selectedSkills.includes(dic.skill))
-            .filter(dic => !diceLogBaseArr.includes(dic));
+            .filter(rollData => setting.value.selectedSkills.includes(rollData.skill))
+            .filter(rollData => !diceLogBaseArr.includes(rollData));
           diceLogBaseArr.push(...selectedArr);
         }
       }
@@ -331,7 +334,7 @@ const rootApp = createApp({
         const totalE = targetRoll.reduce((ac,cur) => ac + cur.E, 0);
         const totalSuc  = targetRoll.reduce((ac,cur) => ac + cur.suc, 0);
         const totalSucE = targetRoll.reduce((ac,cur) => ac + cur.sucE, 0);
-        const total = targetRoll.reduce((ac,cur) => ac+cur.diceVal.reduce((ac2,cur2)=>ac2+cur2,0),0);
+        const total = targetRoll.reduce((ac,cur) => ac + cur.sumDiceVal,0);
 
         totalData.set('ダイス', totalD);
         totalData.set('出目平均', floatRound(total/totalD, 2) || null);
@@ -340,7 +343,7 @@ const rootApp = createApp({
         totalData.set('成功数', totalSuc);
         totalData.set('→期待値', floatRound(totalSucE, 1));
 
-        diceLogBaseArr.push(...structuredClone(targetRoll));
+        diceLogBaseArr.push(...targetRoll);
       }
 
       // -----------
@@ -349,28 +352,17 @@ const rootApp = createApp({
       if (setting.value.sortStyle==='skill') diceLogBaseArr.sort((a,b) => a.skill > b.skill ? 1 : -1);
       else if (setting.value.sortStyle==='rollNo') diceLogBaseArr.sort((a,b) => a.rollNo - b.rollNo);
 
-      const diceLogArr = diceLogBaseArr.map(dic=>{
-        if (isCoC.value) {
-          if (setting.value.showStyle==='log') return [dic.tab, dic.log, dic.CF];
-          else return [dic.tab, dic.skill, dic.threshold, dic.diceVal, dic.CF];
+      const diceLogArr = diceLogBaseArr.map(rollData=>{
+        if (isCoc.value) {
+          if (setting.value.showStyle==='log') return [rollData.tab, rollData.log, rollData.CF];
+          else return [rollData.tab, rollData.skill, rollData.threshold, rollData.diceVal, rollData.CF];
         } else {
-          if (setting.value.showStyle==='log') return [dic.tab, dic.log];
-          else return [dic.tab, dic.skill, `${dic.D}DM<=${dic.threshold}`, dic.C||null, dic.E||null, dic.suc, dic.sucE];
+          if (setting.value.showStyle==='log') return [rollData.tab, rollData.log];
+          else return [rollData.tab, rollData.skill, `${rollData.D}DM<=${rollData.threshold}`, rollData.C||null, rollData.E||null, rollData.suc, rollData.sucE];
         }
       });
 
       return {totalData: totalData, diceLog: diceLogArr};
-
-      /**
-       * 小数点第n位で四捨五入する関数
-       * @param {number} num 元の数値
-       * @param {int} digit 四捨五入する桁数
-       * @returns
-       */
-      function floatRound(num, digit) {
-        const levarage = Math.pow(10,digit);
-        return Math.round(num*levarage)/levarage;
-      }
     }
 
     function deleteLog (e) {
@@ -400,11 +392,14 @@ const rootApp = createApp({
 
 
     onMounted(async () => {
-      const json = await fetch('./setting.json').then(res=>res.json());
-      initSkills.value.coc6th = new Map(json.coc6th);
-      initSkills.value.coc7th = new Map(json.coc7th);
+      setting.value = await fetch('./data/setting.json').then(res=>res.json());
 
-      document.querySelector('footer table tbody').innerHTML = json.changeLog
+      const initSkillJson = await fetch('./data/init-skills.json').then(res=>res.json());
+      initSkills.value.coc6th = new Map(initSkillJson.coc6th);
+      initSkills.value.coc7th = new Map(initSkillJson.coc7th);
+
+      const changeLogJson = await fetch('./data/change-log.json').then(res=>res.json());
+      document.querySelector('footer table tbody').innerHTML = changeLogJson
       .reduce((acc, cur) => acc += `<tr><td>${cur.date}</td><td>${cur.version}</td><td>${cur.detail}</td></tr>`, '');
     });
 
@@ -412,7 +407,7 @@ const rootApp = createApp({
 
     return {
       system,
-      isCoC,
+      isCoc,
       setting,
       initSkills,
 
---
+++ components/class.js
@@ -0,0 +1,129 @@
+export class ChatData {
+  constructor({
+    tab = '',
+    name = '',
+    log = '',
+  } = {}) {
+    this.tab = tab;
+    this.name = name;
+    this.log = log;
+  }
+  get nameNodup() {return this.name.replace(/ *\(\d+\)$/,'');}
+  clone() {
+    return new ChatData({
+      tab: this.tab,
+      name: this.name,
+      log: this.log
+    });
+  }
+}
+
+
+class RollData extends ChatData {
+  constructor({
+    tab = '',
+    name = '',
+    log = '',
+    rollNo = null,
+    skill = '',
+    threshold = null,
+  }={}) {
+    super({
+      tab: tab,
+      name: name,
+      log: log,
+    });
+    this.rollNo = rollNo;
+    this.skill = skill;
+    this.threshold = threshold;
+  }
+  clone() {
+    return new RollData({
+      tab: this.tab,
+      name: this.name,
+      log: this.log,
+      rollNo: this.rollNo,
+      skill: this.skill,
+      threshold: this.threshold,
+    })
+  }
+}
+
+
+export class CocRollData extends RollData {
+  constructor({
+    tab = '',
+    name = '',
+    log = '',
+    rollNo = null,
+    skill = '',
+    threshold = null,
+    diceVal = null,
+    CF = '',
+  } = {}) {
+    super({
+      tab : tab,
+      name : name,
+      log : log,
+      rollNo : rollNo,
+      skill : skill,
+      threshold : threshold,
+    });
+    this.diceVal = diceVal;
+    this.CF = CF;
+  }
+  clone() {
+    return new CocRollData({
+      tab: this.tab,
+      name: this.name,
+      log: this.log,
+      rollNo: this.rollNo,
+      skill: this.skill,
+      threshold: this.threshold,
+      diceVal: this.diceVal,
+      CF: this.CF,
+    })
+  }
+}
+
+
+export class EmokloreRollData extends RollData {
+  constructor({
+    tab = '',
+    name = '',
+    log = '',
+    rollNo = null,
+    skill = '',
+    threshold = null,
+    diceVal = [],
+  } = {}) {
+    super({
+      rollNo : rollNo,
+      tab : tab,
+      name : name,
+      log : log,
+      diceVal : diceVal,
+      skill : skill,
+      threshold : threshold,
+    });
+    this.diceVal = [...diceVal];
+  }
+  get D() {return this.diceVal.length;}
+  get C() {return this.diceVal.filter(e => e===1).length;}
+  get E() {return this.diceVal.filter(e => e===10).length;}
+  get suc() {return this.C - this.E + this.diceVal.filter(e => e<=this.threshold).length;}
+  get sucE() {return this.D * this.threshold / 10;}
+  get sumDiceVal() {return this.diceVal.reduce((acc,cur)=>acc+cur,0);}
+
+  clone() {
+    return new EmokloreRollData({
+      tab: this.tab,
+      name: this.name,
+      log: this.log,
+      rollNo: this.rollNo,
+      skill: this.skill,
+      threshold: this.threshold,
+      diceVal: this.diceVal,
+    })
+  }
+}
